797d56ddd8623da90a1d41825f69b36cf8ce5806
Sumit9328 <sumitdhir171@gmail.com>
2025-03-28 11:45:49 +0530

    final

--- script.js
+++ script.js
@@ -67,15 +67,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.querySelector(".menu-icon");
   const closeIcon = document.querySelector(".close-icon");
   const mobileNav = document.querySelector(".mobile-nav");
+  const body = document.body
   menuIcon.addEventListener("click", function () {
       mobileNav.classList.add("active");
       menuIcon.style.display = "none";
       closeIcon.style.display = "block";
+      body.classList.add("no-scroll")
   });
   closeIcon.addEventListener("click", function () {
       mobileNav.classList.remove("active");
       closeIcon.style.display = "none";
       menuIcon.style.display = "block";
+      body.classList.remove("no-scroll")
   });
 });
 
